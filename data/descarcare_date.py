1ab55a243c910669a525e3cb834c21a978325516
Edi-Laptop_Munca <alexandru.vlase@visma.com>
2026-01-14 18:08:47 +0200

    Varinata Finala Ica

---
+++ data/descarcare_date.py
@@ -0,0 +1,6 @@
+from roboflow import Roboflow 
+
+rf = Roboflow(api_key="0IvyJ3dhBaYzm81uf99D") 
+project = rf.workspace("datasetforpython").project("dataset-labeling-nmkkk") 
+version = project.version(1) 
+dataset = version.download("yolov8")
